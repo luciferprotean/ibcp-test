5cb1994454a2fdf05d6800aab0b32e08cb5ad93c
SharadC <avalontech@gmail.com>
2025-04-17 18:27:09 +0530

    first streamlit lines

--- python_summative.py
+++ python_summative.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import csv
+import streamlit as st
 
 df = pd.read_csv('athlete_events.csv')
 df_gold = df[df['Medal'] == 'Gold']
@@ -12,4 +13,10 @@ top_10_countries = athlete_counts.head(20)
 print(top_10_countries)
 max_country = athlete_counts.idxmax()
 print(max_country)
+st.write("""
+#IBCP Capstone
+#Test deployment
+Checking *italics* and line writes
+""")
+st.line_chart(df)
 
