66eaf125b15ffc8f6ee96176135b5193b98ffff7
SharadC <avalontech@gmail.com>
2025-04-17 18:38:09 +0530

    modified the dataframe

--- python_summative.py
+++ python_summative.py
@@ -9,14 +9,17 @@ df_gold = df[df['Medal'] == 'Gold']
 print(df_gold.head())
 athlete_counts = df_gold['Team'].value_counts()
 print(athlete_counts)
+athlete_counts_df = df_gold['Team'].value_counts().reset_index()
+athlete_counts_df.columns = ['Team', 'Count']
+print(athlete_counts_df.head())
 top_10_countries = athlete_counts.head(20)
 print(top_10_countries)
 max_country = athlete_counts.idxmax()
 print(max_country)
-st.write("""
-#IBCP Capstone
-#Test deployment
-Checking *italics* and line writes
-""")
-st.line_chart(df_gold)
+# st.write("""
+# #IBCP Capstone
+# #Test deployment
+# Checking *italics* and line writes
+# """)
+# st.line_chart(df_gold)
 
